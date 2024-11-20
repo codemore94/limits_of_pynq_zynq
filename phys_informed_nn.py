b8f462178a06d064c6fc67c04417b5754cf2cee7
Mikko Saari <mikko.l.saari@tuni.fi>
2024-11-21 01:54:11 +0200

    phys3

--- phys_informed_nn.py
+++ phys_informed_nn.py
@@ -10,7 +10,7 @@ from torch.func import functional_call, grad, vmap
 
 class LinearNN(nn.Module):
     def __init__(self,
-                 num_inputs: int = 1
+                 num_inputs: int = 1,
                  num_layers: int = 1,
                  num_neurons: int = 5,
                  act: nn.Module = nn.Tanh())->None:
@@ -37,16 +37,91 @@ class LinearNN(nn.Module):
             else:
                 params_dict = params
                 return functional_call(model,params_dict,(x,))
-    fns = []
-    fns.append(f)
-    dfunc = f
-    for _ in range(derivative_order):
-        dfunc = grad(dfunc)
-        dfunc_vmap = vmap(dfunc,in_dims = (0,None))
-        fns.append(dfunc_vmap)
-    return fns
+        fns = []
+        fns.append(f)
+        dfunc = f
+        for _ in range(derivative_order):
+            dfunc = grad(dfunc)
+            dfunc_vmap = vmap(dfunc,in_dims = (0,None))
+            fns.append(dfunc_vmap)
+        return fns
 
-def tuple_to_dict_parameters():
+def tuple_to_dict_parameters(
+    model: nn.Module, params: tuple[torch.nn.Parameter,...]
+    ) -> OrderedDict[str,torch.nn.Parameter]:
+    keys = list(dict(model.named_parameters()).keys())
+    values = list(params)
+    return OrderedDict(({k:v for k,v in zip(keys,values)}))
+    
+if __name__ == "__main__":
+    model = LinearNN(num_layers=2)
+    fns = make_forward_fn(model,derivative_order=2)
+    
+    batch_size = 10
+    x = torch.randn(batch_size)
+    params = dict(model.named_parameters())
+    
+    fn_x = fns[0](x,params)
+    assert fn_x.shape[0] == batch_size
 
+    dfn_x = fns[1](x,params)
+    assert dfn_x.shape[0] == batch_size
 
-  
+    ddfn_x = fns[2](x,params)
+    assert ddfn_x.shape[0] == batch_size
+
+    def make_loss_fn(f: Callable,dfdx: Callable) -> Callable:
+        def loss_fn(params: torch.Tensor, x: Torch.tensor):
+            
+            #interior loss
+            f_value = f(x,params)
+            interior = dfdx(x,params) -R*f_value*(1-f_value)
+            
+            #boundary loss
+
+            x0 = X_B
+            f0 = F_B
+            x_boundary = torch.tensor([x0])
+            f_boundary = torch.tensor([f0])
+
+            boundary = f(x_boundary,params) -f_boundary
+            loss = nn.MSELoss()
+            loss_value = loss(interior,torch.zeros_like(interior)) + loss(boundary,torch.zeros_like(boundary))
+            return loss_value
+        
+        return loss_fn
+
+    torch.manual_seed(42)
+    parser = argparse.ArgumentParser()
+    parser.add_argument("-n","--num-hidden",type = int, default = 5)
+    parser.add_argument("-d","--dim-hidden",type = int, default = 5)
+    parser.add_argument("-b","--batch-size",type = int, default = 30)
+    parser.add_argument("-lr","--learning-rate",type = float, default = 1e-1)
+    parser.add_argument("-e","--num-epochs",type = int, default = 100)
+    args,unknown = parser.parse_known_args()
+    num_hidden = args.num_hidden
+    dim_hidden = args.dim_hidden
+    batch_size = args.bastch_size
+    num_iter = args.num_epochs
+    tolerance = 1e-8
+    learning_rate = args.learning_rate
+    domain = (-5.0,5.0)
+
+    model = LInearNN(num_layers=num_hidden, num_neurons = dim_hidden,num_inputs=1)
+    funcs = make_forward_fn(model,derivative_order = 1) 
+    f = funcs[0]
+    dfdx = funcs[1]
+    loss_fn = make_loss_fn(f,dfdx)
+    optimizer = torchopt.FuncOptimizer(torchopt.adam(lr=learning_rate))
+    params = tuple(model.parameters())
+    loss_evolution = []
+for i in range(num_iter):
+    x = torch.FloatTensor(batch_size).uniform_(domain[0],domain[1])
+    loss = loss_fn(params,x)
+    params = optimizer.step(loss,params)
+    print(f"Iteration {i} with loss {float(loss)}")
+    loss_evolution.append(float(loss))
+
+x_eval = torch.linspace(domain[0],domain[1],steps=100).reshape(-1,1)
+f_eval = f(x_eval,params)
+

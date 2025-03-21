8f6a2bacd4bf558742c4bfdc85cadb84ef2238e4
Mikko Saari <mikko.l.saari@tuni.fi>
2025-03-21 11:18:15 +0200

    verilog_version

---
+++ top_level.v
@@ -0,0 +1,15 @@
+module Transformer(
+		   input clk,
+		   input rst,
+		   input reg[DATA_WIDTH-1:0],
+		   output reg[DATA_WIDTH-1:0]
+);
+
+   reg					     attention_out[DATA_WIDTH-1:0];
+   reg					     ff_out[DATA_WIDTH-1:0];
+   reg					     norm_out[DATA_WIDTH-1:0];
+end module;
+   
+   
+   
+   

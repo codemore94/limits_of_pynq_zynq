03f49de8f6d1ff14b198576bda6b036cdc06ea89
Mikko Saari <mikkolh.saari@gmail.com>
2025-04-27 00:19:24 +0300

    the_most_useful_search_command_in_bash

---
+++ the_most_useful_search_command_in_bash.sh
@@ -0,0 +1,5 @@
+find -name \*. -print0 | xargs -0 grep -l '' | more
+#file-type   ^                             ^  
+	     |                             |                                                                          put file-type                 put names to be searched
+		 
+		 

7b706f2df0b842b054ce9e83fac16f4635381de2
Mikko Saari <mikkolh.saari@gmail.com>
2025-04-27 00:24:49 +0300

    more_info

--- the_most_useful_search_command_in_bash.sh
+++ the_most_useful_search_command_in_bash.sh
@@ -3,3 +3,4 @@ find -name \*. -print0 | xargs -0 grep -l '' | more
 	     |                             |                                                                          put file-type                 put names to be searched
 		 
 		 
+#actually not invented by me but some wise guy in stackoverflow. This command makes your Linux day better :D
